22cc0ea11a7817f3f00494e71d207a48668323ba
Anon <Anon@DESKTOP-DTLM9AJ>
2026-03-12 17:18:44 +0300

    Задача 1. Время суток

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Task1()
+        {
+            var currentHour = DateTime.Now.Hour;
+            string s;
+
+            if (currentHour < 6) s = "Доброй ночи";
+
+            else if (currentHour < 12) s =  "Доброе утро";
+
+            else if (currentHour < 18) s = "Добрый день";
+
+            else s = "Добрый вечер";
+            
+            return s;
+        }
+    }
+}
